a400660af84a16f770b4f20d97924710e97cfca5
Batuhan Yigit <batuhanyigit1705@gmail.com>
2021-10-02 00:52:32 +0300

    Initial commit

---
+++ Game/Program.cs
@@ -0,0 +1,25 @@
+﻿using Raylib_cs;
+
+namespace Game
+{
+    static class Program
+    {
+        public static void Main()
+        {
+            Raylib.InitWindow(800, 480, "Ludum Dare #49");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.DrawText("Its working!", 12, 12, 20, Color.BLACK);
+                Raylib.DrawRect()
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# LD49
+ A game made for Ludum Dare #49 in C# using Raylib
