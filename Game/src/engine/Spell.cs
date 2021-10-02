5990c65284d9c002a927f6c8e6bdc172ff65924f
lolchicer <74567272+lolchicer@users.noreply.github.com>
2021-10-02 19:21:17 +0300

    Add files via upload
    caution: i could add excessive [] overloads and wrong gamedesign plan interpretations

---
+++ Game/src/engine/Spell.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public abstract class Spell
+    {
+        Genes genes;
+        public double this[int i]
+        {
+            get { return genes[i]; }
+            set { genes[i] = value; }
+        }
+
+        public Spell(Genes genes) {
+            this.genes = genes;
+        }
+
+        public void Use(int x, int y)
+        {
+            Influence(UseGeneral(x, y));
+        }
+        protected abstract void Influence(Unit influensing);
+        protected abstract Unit UseGeneral(int x, int y);
+    }
+}
---
+++ Game/src/engine/Unit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public abstract class Unit : Entity
+    {
+        List<Spell> spells;
+        public Spell this[int i]
+        {
+            get { return spells[i]; }
+            set { spells[i] = value; }
+        }
+
+        public Unit(Transform transform, EntityTag tag = EntityTag.ENEMY, string name = "Unit")
+            : base(transform, tag, name) { }
+    }
+}
