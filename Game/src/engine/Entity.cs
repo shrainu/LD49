7dd0a0366a2f7bec4f313d85af625e70e8bf1cc6
lolchicer <lolchicer@live.ru>
2021-10-03 21:24:43 +0300

    gene colors
    hello i gonna kill everyone with cringe
    added:
     a function to get a gene color (using a class from system.drawing – should fix it later);
     >, < operators for genes;
     a folder for functions;
     some functions for colors and vectors;
    changed:
     moved perklist from unit to entity.

--- Game/src/engine/Entity.cs
+++ Game/src/engine/Entity.cs
@@ -1,4 +1,5 @@
 using System.Numerics;
+using System.Collections.Generic;
 using Raylib_cs;
 
 
---
+++ Game/src/functions/Vector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public static class VectorFunctions
+    {
+        public static double Length(Vector2 vector)
+        {
+            return Math.Sqrt((vector.x * vector.x) + (vector.y * vector.y));
+        }
+        public static double Degree(Vector2 vector)
+        {
+            return Math.Asin(Length(vector)/vector.x);
+        }
+        public static double Degree(double hypotenuse, float adjacent)
+        {
+            return Math.Asin(hypotenuse / adjacent);
+        }
+    }
+}
