4060e0e2ef90914b3d049b5b26a1045c9d1f37be
Batuhan Yigit <batuhanyigit1705@gmail.com>
2021-10-04 19:58:44 +0300

    Dungeon Generation Corridors

--- Game/src/scenes/SceneTest.cs
+++ Game/src/scenes/SceneTest.cs
@@ -24,9 +24,8 @@ namespace Game {
         public SceneTest(SceneID sceneId) : base(sceneId) {
 
             // Initialize Tilemap
-            int[,] dungeon = DungeonGenerator.GenerateDungeon(40, 40, 12, new Vector2int(4, 4), new Vector2int(8, 8));
             tilemap = new Tilemap(40, 40, 16, "res/tileset_back.png", "res/tileset_front.png");
-            tilemap.SetData(dungeon);
+            tilemap.GenerateNewMap(12, new Vector2int(4, 4), new Vector2int(8, 8));
 
             // Initialize Entity Manager
             entityManager = new EntityManager();
@@ -49,8 +48,7 @@ namespace Game {
             entityManager.Events();
 
             if (Raylib.IsKeyPressed(KeyboardKey.KEY_R)) {
-                int[,] dungeon = DungeonGenerator.GenerateDungeon(40, 40, 12, new Vector2int(4, 4), new Vector2int(8, 8));
-                tilemap.SetData(dungeon);
+                tilemap.GenerateNewMap(12, new Vector2int(4, 4), new Vector2int(8, 8));
             }
         }
         public override void Update() {
